8a7fb861ff359cf79b562701a6fd5c19a2167e6e
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2020-09-28 14:26:57 -0500

    Ya funciona el área del triángulo

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hola Mundo")
+"""
+Carlos Paredes Márquez.
+Área de un triángulo.
+29/09/2020.
+"""
+print("Área de un triángulo.\n\n\n")
+
+base = int(input("base ->    "))
+altura = int(input("altura ->    "))
+area = ((base * altura) / 2)
+
+print(area)
